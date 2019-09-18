c214b43cca3f15be90df1f1666f5781e23a6f9ae
Carmine Biello <nuccio@MacBook-Pro-di-Carmine.local>
2019-09-18 05:14:55 +0200

    Trasformato in http

--- server.js
+++ server.js
@@ -3,17 +3,23 @@
 var express = require('express');
 var app = express();
 const path = require('path');
-var https = require('https');
+//var https = require('https');
+var http = require('http');
 var fs = require('fs');
 
-const options = {
-  key: fs.readFileSync('chiave_privata.pem'),
-  cert: fs.readFileSync('certificato.pem')
-};
+//const options = {
+// key: fs.readFileSync('chiave_privata.pem'),
+//  cert: fs.readFileSync('certificato.pem')
+//};
 
 app.use(express.static(__dirname));
 
-https.createServer(options, app).listen(process.env.PORT || 4000, function(){
-  console.log('Your node js server is running');
-}
-);
+//https.createServer(options, app).listen(process.env.PORT || 4000, function(){
+//  console.log('Your node js server is running');
+//}
+//);
+
+http.createServer(app).listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
+  }
+  );
